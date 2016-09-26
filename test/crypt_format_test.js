72e1f67d566a4a99a9810f9c2a262ef68aa9f8b4
Taka Okunishi <okunishinishi@gmail.com>
2016-09-26 17:33:12 +0900

    Version incremented to 0.3.2

---
+++ test/crypt_format_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for cryptFormat.
+ * Runs with mocha.
+ */
+'use strict'
+
+const cryptFormat = require('../lib/crypt_format.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('crypt-format', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Crypt format', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
