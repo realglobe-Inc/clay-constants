9de46c731a4b19dd5fde28c4c41f36baedc5e1fd
Taka Okunishi <okunishinish@gmail.com>
2016-11-13 10:11:26 +0900

    Add rest api path

---
+++ test/rest_paths_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for restPaths.
+ * Runs with mocha.
+ */
+'use strict'
+
+const restPaths = require('../lib/rest_paths.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('rest-paths', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Rest paths', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
